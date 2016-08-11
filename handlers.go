f40f65a0f59a3581a4c7aa820ccd83de1e0d5cf5
Michael Leikind <mleikind@pinterest.com>
2016-08-11 12:41:52 -0700

    init

---
+++ handlers.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome!")
+}
+
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(todos); err != nil {
+		panic(err)
+	}
+}
+
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	todoId := vars["todoId"]
+	fmt.Fprintf(w, "TodoShow:", todoId)
+}
---
+++ repo.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+var currentId int
+
+var todos Todos
+
+func int() {
+	RepoCreateTodo(Todo{name: "write shit"})
+	RepoCreateTodo(Todo{name: "write another shit"})
+}
+
+func RepoFindTodo(id int) Todo {
+	for _, t := range todos {
+		if t.Id == id {
+			return t
+		}
+	}
+	// return empty Todo if not found
+	return Todo{}
+}
+
+func RepoCreateTodo(t Todo) Todo {
+	currentId += 1
+	t.Id = currentId
+	todos = append(todos, t)
+	return t
+}
+
+func RepoDestroyTodo(id int) error {
+	for i, t := range todos {
+		if t.Id == id {
+			todos = append(todos[:i], todos[i+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("Could not find Todo with id of %d to delete", id)
+}
